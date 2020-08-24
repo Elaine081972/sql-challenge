bd80cf9b1034584743b8103c50eed9aa0b597b2e
Elaine Hamilton <elainehamilton@elaines-mbp.lan>
2020-08-24 09:16:45 -0700

    update query.sql file

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -13,14 +13,24 @@ employees.emp_no = salaries.emp_no;
 
 -- List first name, last name and hire date for employees who were hired in 1986
 
-SELECT 
+SELECT first_name, 
+   last_name, 
+   hire_date
+FROM employees
+WHERE
+    hire_date >= '1986-01-01'
+    AND hire_date <= '1986-12-31'
 
-
-
-
-
-
-
---list the manager of each department with the following info: dept. number, dept. name, manager's emp 
+-- List the manager of each department with the following info: dept. number, dept. name, manager's emp 
 --number last name, first name
 
+
+
+
+
+
+
+-- List the department of each employee with the following info: employee #, last name, first name, and 
+-- department name
+
+SELECT 
