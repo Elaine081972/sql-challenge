0e9343e0d0296e84ec5ebb14b28de944060d7b41
Elaine Hamilton <elainehamilton@Elaines-MacBook-Pro.local>
2020-08-24 12:26:16 -0700

    update query

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -24,27 +24,25 @@ WHERE
 --3.List the manager of each department with the following info: dept. number, dept. name,  
 -- manager's emp number, last name, first name
 
--- Alternative solution:
--- Perform an INNER JOIN on the two tables
-SELECT p.first_name, p.last_name, p.hand, m.loser_rank
-FROM matches AS m
-INNER JOIN players AS p ON
-p.player_id=m.loser_id;
- 
-
-
-
-
+SELECT d.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+FROM departments d 
+INNER JOIN dept_emp c
+ON d.dept_no = c.dept_no
+INNER JOIN employee e
+ON c.emp_no = e.emp_no
+INNER JOIN titles t
+ON e.title_id = t.title_id
+WHERE title = 'Manager';
 
 --4. List the department of each employee with the following info: employee #, last name, first name, and 
 -- department name
 
-
-
-
-
-
-
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+INNER JOIN dept_emp c
+ON e.emp_no = c.emp_no
+INNER JOIN departments d
+ON c.dept_no = d.dept_no;
 
 --5. List first name, last name, and sex for employees whose first name is "Hercules" and last names 
 -- begin with "B".
