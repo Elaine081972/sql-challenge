cb82b3ed80a28013783eb3c8846116ffc38cf480
Elaine Hamilton <elainehamilton@elaines-mbp.lan>
2020-08-23 19:39:24 -0700

    to update schema file

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,89 +1,49 @@
 -- Create schemas
 -- Create tables
 CREATE TABLE departments (
-    dept_no VARCHAR(10),
-    dept_name VARCHAR(30)   
+    dept_no VARCHAR(10) NOT NULL,
+    dept_name VARCHAR(30),   
     PRIMARY KEY (dept_no)
 );
-
+-- made both primary keysss - makes composite? NEED To check - employees definitely in multiple depts
 CREATE TABLE dept_emp (
-    emp_no INTEGER,
-    dept_no VARCHAR(10),
-    PRIMARY KEY (emp_no)
-    PRIMARY KEY (dept_no),  
+    emp_no INTEGER NOT NULL,
+    dept_no VARCHAR(10) NOT NULL,
+    PRIMARY KEY (emp_no, dept_no)
+      
 );
--- Let's create a third table with a foreign key that references the first table
+
 CREATE TABLE dept_manager (
-    dept_no VARCHAR(10),
-    emp_no INTEGER,
+    dept_no VARCHAR(10) NOT NULL,
+    emp_no INTEGER NOT NULL,
+    PRIMARY KEY (dept_no, emp_no)
+    
+);    
+  
+CREATE TABLE employees (
+    emp_no INTEGER NOT NULL,
+    title_id VARCHAR(10) NOT NULL,
+    birth_date DATE,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    sex VARCHAR(10),
+    hire_date DATE,
+    PRIMARY KEY (emp_no),
+    FOREIGN KEY (title_id) REFERENCES titles(title_id)
+);
+
+CREATE TABLE salaries (
+    emp_no INTEGER NOT NULL,
+    salary INTEGER,
+    PRIMARY KEY (emp_no)
    
-    PRIMARY KEY (id),
-    FOREIGN KEY (customer_id) REFERENCES customer(id)
+);
+
+CREATE TABLE titles (
+  
+    title_id VARCHAR(10) NOT NULL,
+    title VARCHAR(30),
+    PRIMARY KEY (title_id)
 );
 
 
-
--- Populate that table
-INSERT INTO customer (first_name, last_name)
-VALUES
-  ('Bob', 'Smith'),
-  ('Jane', 'Davidson'),
-  ('Jimmy', 'Bell'),
-  ('Stephanie', 'Duke');
-
--- View our table
-SELECT * FROM customer;
-
--- 2. Create Customer Email table
-CREATE TABLE customer_email (
-    id SERIAL,
-    email VARCHAR(30) NOT NULL,
-    customer_id INTEGER NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (customer_id) REFERENCES customer(id)
-);
-
--- Populate that table
-INSERT INTO customer_email (customer_id, email)
-VALUES
-  (1, 'bobsmith@email.com'),
-  (2, 'jdavids@email.com'),
-  (3, 'jimmyb@email.com'),
-  (4, 'sd@email.com');
-
--- View our second table
-SELECT * FROM customer_email;
-
--- Let's create a third table with a foreign key that references the first table
-CREATE TABLE customer_phone (
-    id SERIAL,
-    phone VARCHAR(30) NOT NULL,
-    customer_id INTEGER NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (customer_id) REFERENCES customer(id)
-);
-
--- Populate that table
-INSERT INTO customer_phone (customer_id, phone)
-VALUES
-  (1, '435-344-2245'),
-  (2, '332-776-4678'),
-  (3, '221-634-7753'),
-  (4, '445-663-5799');
-
--- View our third table
-SELECT * FROM customer_phone;
-
-
--- Uncomment the following to demonstrate the error of inserting a value with no foreign key
--- INSERT INTO customer_phone(customer_id, phone)
--- VALUES
-  -- (10, '555-444-3333');
-
--- Join Tables
-SELECT customer.first_name, customer.last_name, email.email, phone.phone
-FROM customer
-JOIN customer_email AS email
-  ON customer.id = email.customer_id
-JOIN customer_phone AS phone
-  ON customer.id = phone.customer_id;
